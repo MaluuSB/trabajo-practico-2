5d1c92e55c5a536d679963bc7094d12460d1315f
Malen Sendon Buzzi <maluulp588@gmail.com>
2023-11-06 23:10:42 -0300

    mensaje de commit

--- tp2_orm/src/main/java/app/models/TipoUsuario.java
+++ tp2_orm/src/main/java/app/models/TipoUsuario.java
@@ -4,9 +4,11 @@ package app.models;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.Table;
 
+// Anotacion que indica que esta clase representa una tabla llamada "tipo_usuario" en la base de datos
 @Table("tipo_usuario")
 public class TipoUsuario extends Model{
     
+    // Definicion de constantes para tipos de usuario
     public static final String SISTEMA = "Sistema";
     public static final String ADMINISTRACION = "Administracion";
     
